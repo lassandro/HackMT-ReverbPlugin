b2fefe3ee3669fa806305b1dd3e809fb182fd766
Josh Morris <josh.e.morris@gmail.com>
2017-02-04 12:57:03 -0600

    add comb filter implementation

---
+++ Source/Processors/CombFilter.cpp
@@ -0,0 +1,36 @@
+#include "CombFilter.h"
+
+void CombFilter::setSize(const int & size)
+{
+	if (size != buffer.size()) {
+		bufferIndex = 0;
+		buffer.resize(size);
+	}
+}
+
+void CombFilter::clear()
+{
+	lastCombOut = 0;
+	for (auto iter = buffer.begin(); auto != buffer.end(); ++iter) {
+		(*iter) = 0;
+	}
+}
+
+float CombFilter::process(const float input, const float damp, const float feedbacklvl)
+{
+	// read element at current position in filter buffer
+	const float output = buffer[bufferIndex];
+	float filterOut;
+
+	// implement LPF in feedback to simulate room damping
+	lastCombOut = (output * (1.0 - damp)) + (lastCombOut * damp);
+	
+	// implement comb filter
+	filterOut = input + (lastCombOut *feedbacklvl);
+
+	// write filter output to buffer and inc index
+	buffer[bufferIndex] = filterOut;
+	bufferIndex = (bufferIndex + 1) % buffer.size();
+
+	return filterOut;
+}
--- Source/Processors/CombFilter.h
+++ Source/Processors/CombFilter.h
@@ -1,1 +1,22 @@
 #pragma once
+#ifndef COMBFILTER_H
+#define COMBFILTER_H
+
+#include <vector>
+
+using std::vector;
+
+class CombFilter {
+public:
+	CombFilter() : bufferIndex(0), lastCombOut(0) {};
+	void setSize(const int& size);
+	void clear();
+	float process(const float input, const float damp, const float feedbacklvl);
+private:
+	vector<float> buffer;
+	int bufferIndex;
+	float lastCombOut;
+};
+
+
+#endif
