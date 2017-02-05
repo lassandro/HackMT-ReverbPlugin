7b68a7f6e5821b3f43e0e521a7ad334c3af5b517
Antonio Lassandro <lassandroan@gmail.com>
2017-02-04 23:52:12 -0600

    Hardcoded Knob Size

--- Source/Components/Knob.cpp
+++ Source/Components/Knob.cpp
@@ -4,14 +4,12 @@ Knob::Knob()
 {
 
     setSliderStyle(juce::Slider::RotaryHorizontalVerticalDrag);
-
+    setSize(96, 96);
     background = ImageCache::getFromMemory(BinaryData::Knob_png, BinaryData::Knob_pngSize);
 
 }
 
 void Knob::paint(Graphics& g)
 {
-
     g.drawImageAt(background, 0, 0);
-
 }
